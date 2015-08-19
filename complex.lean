d30257dc6f06b58dc2e1fa923d69c827a194ee06
Eric McCorkle <emc2@metricspace.net>
2015-08-18 22:25:30 -0400

    Added distributive laws

--- complex.lean
+++ complex.lean
@@ -7,14 +7,14 @@ namespace algebra
 
 open prod ring
 
-definition complex_ty [reducible] (A : Type) : Type := A × A
+definition complex_ty (A : Type) : Type := A × A
 
 structure complex [class] (A : Type) :=
-  (ty : Type)
-  (real : A -> ty)
-  (i : ty)
-  (complex_conj : ty → ty)
-  (abs : ty -> A)
+  (ℂ : Type)
+  (real : A -> ℂ)
+  (i : ℂ)
+  (complex_conj : ℂ → ℂ)
+  (abs : ℂ -> A)
 
 namespace complex_ty
 
@@ -24,7 +24,7 @@ definition complex_conj_impl {A : Type} [hn : has_neg A] :
                              complex_ty A → complex_ty A
   | (rval, ival) := (rval, -ival)
 
-definition complex_zero [reducible] {A : Type} [hz : has_zero A] :
+definition complex_zero {A : Type} [hz : has_zero A] :
                         complex_ty A := (0, 0)
 
 definition complex_has_zero [instance] {A : Type} [hz : has_zero A] :
@@ -39,10 +39,10 @@ definition complex_has_one [instance] {A : Type}
                            has_one (complex_ty A) :=
   ⦃ has_one, one := complex_one ⦄
 
-definition complex_i [reducible] {A : Type} [hz : has_zero A] [hz : has_one A] :
+definition complex_i {A : Type} [hz : has_zero A] [hz : has_one A] :
                      complex_ty A := (0, 1)
 
-definition complex_add [reducible] {A : Type} [ha : has_add A]
+definition complex_add {A : Type} [ha : has_add A]
                        (v1 : complex_ty A) (v2 : complex_ty A) : complex_ty A :=
   let r1 : A := pr1 v1,
       r2 : A := pr1 v2,
@@ -55,8 +55,7 @@ definition complex_has_add [instance] {A : Type} [ha : has_add A] :
                            has_add (complex_ty A) :=
   ⦃ has_add, add := complex_add ⦄
 
-definition complex_mul [reducible] {A : Type}
-                       [ag : add_group A] [hm : has_mul A]
+definition complex_mul {A : Type} [ag : add_group A] [hm : has_mul A]
                        (v1 : complex_ty A) (v2 : complex_ty A) :
     complex_ty A :=
   let r1 := pr1 v1,
@@ -82,9 +81,9 @@ definition complex_has_neg [instance] {A : Type} [hn : has_neg A] :
                            has_neg (complex_ty A) :=
   ⦃ has_neg, neg := complex_neg ⦄
 
-definition complex_inv [reducible] {A : Type} [hi : has_inv A] [ha : has_add A]
-                                   [hm : has_mul A] [hn : has_neg A]
-                                   (c : complex_ty A) : complex_ty A :=
+definition complex_inv {A : Type} [hi : has_inv A] [ha : has_add A]
+                       [hm : has_mul A] [hn : has_neg A]
+                       (c : complex_ty A) : complex_ty A :=
   let r := pr1 c,
       i := pr2 c
   in
@@ -349,6 +348,98 @@ definition complex_comm_monoid [instance] {A : Type} [r : comm_ring A] :
                  mul_comm := complex_mul_comm,
                  one_mul := complex_one_mul,
                  mul_one := complex_mul_one ⦄
+
+theorem complex_left_distrib {A : Type} [cr : comm_ring A] :
+  ∀ (c1 c2 c3 : complex_ty A), complex_mul c1 (complex_add c2 c3) =
+                               complex_add (complex_mul c1 c2)
+                                           (complex_mul c1 c3) :=
+  take c1 : complex_ty A,
+  take c2 : complex_ty A,
+  take c3 : complex_ty A,
+  let r1 := pr1 c1,
+      r2 := pr1 c2,
+      r3 := pr1 c3,
+      i1 := pr2 c1,
+      i2 := pr2 c2,
+      i3 := pr2 c3
+  in
+    show (r1 * (r2 + r3) - i1 * (i2 + i3),
+          r1 * (i2 + i3) + i1 * (r2 + r3)) =
+         (r1 * r2 - i1 * i2 + (r1 * r3 - i1 * i3),
+          r1 * i2 + i1 * r2 + (r1 * i3 + i1 * r3)),
+    from pair_eq (calc r1 * (r2 + r3) - i1 * (i2 + i3)
+                      = r1 * r2 + r1 * r3 - (i1 * (i2 + i3)) :
+                        by rewrite left_distrib
+                  ... = r1 * r2 + r1 * r3 - (i1 * i2 + i1 * i3) :
+                        by rewrite left_distrib
+                  ... = r1 * r2 + r1 * r3 - i1 * i2 - i1 * i3 :
+                        by rewrite sub_add_eq_sub_sub
+                  ... = r1 * r2 + (r1 * r3 - i1 * i2) - i1 * i3 :
+                        by rewrite *add.assoc
+                  ... = r1 * r2 + (-(i1 * i2) + r1 * r3) - i1 * i3 :
+                        by rewrite -{_ - i1 * i2}neg_add_eq_sub
+                  ... = r1 * r2 - i1 * i2 + (r1 * r3 - i1 * i3) :
+                        by rewrite -*add.assoc)
+                 (calc r1 * (i2 + i3) + i1 * (r2 + r3)
+                      = r1 * i2 + r1 * i3 + (i1 * (r2 + r3)) :
+                        by rewrite left_distrib
+                  ... = r1 * i2 + r1 * i3 + (i1 * r2 + i1 * r3) :
+                        by rewrite left_distrib
+                  ... = r1 * i2 + (r1 * i3 + i1 * r2 + i1 * r3) :
+                        by rewrite *add.assoc
+                  ... = r1 * i2 + (i1 * r2 + r1 * i3 + i1 * r3) :
+                        by rewrite {i1 * r2 + _}add.comm
+                  ... = r1 * i2 + i1 * r2 + (r1 * i3 + i1 * r3) :
+                        by rewrite -*add.assoc)
+
+theorem complex_right_distrib {A : Type} [cr : comm_ring A] :
+  ∀ (c1 c2 c3 : complex_ty A), complex_mul (complex_add c1 c2) c3 =
+                               complex_add (complex_mul c1 c3)
+                                           (complex_mul c2 c3) :=
+  take c1 : complex_ty A,
+  take c2 : complex_ty A,
+  take c3 : complex_ty A,
+  let r1 := pr1 c1,
+      r2 := pr1 c2,
+      r3 := pr1 c3,
+      i1 := pr2 c1,
+      i2 := pr2 c2,
+      i3 := pr2 c3
+  in
+    show ((r1 + r2) * r3 - (i1 + i2) * i3,
+          (r1 + r2) * i3 + (i1 + i2) * r3) =
+         (r1 * r3 - i1 * i3 + (r2 * r3 - i2 * i3),
+          r1 * i3 + i1 * r3 + (r2 * i3 + i2 * r3)),
+    from pair_eq (calc (r1 + r2) * r3 - (i1 + i2) * i3
+                      = r1 * r3 + r2 * r3 - (i1 + i2) * i3 :
+                        by rewrite right_distrib
+                  ... = r1 * r3 + r2 * r3 - (i1 * i3 + i2 * i3) :
+                        by rewrite right_distrib
+                  ... = r1 * r3 + r2 * r3 - i1 * i3 - i2 * i3 :
+                        by rewrite sub_add_eq_sub_sub
+                  ... = r1 * r3 + (r2 * r3 - i1 * i3) - i2 * i3 :
+                        by rewrite *add.assoc
+                  ... = r1 * r3 + (-(i1 * i3) + r2 * r3) - i2 * i3 :
+                        by rewrite -{_ - i1 * i3}neg_add_eq_sub
+                  ... = r1 * r3 - i1 * i3 + (r2 * r3 - i2 * i3) :
+                        by rewrite -*add.assoc)
+                 (calc (r1 + r2) * i3 + (i1 + i2) * r3
+                      = r1 * i3 + r2 * i3 + (i1 + i2) * r3 :
+                        by rewrite right_distrib
+                  ... = r1 * i3 + r2 * i3 + (i1 * r3 + i2 * r3) :
+                        by rewrite right_distrib
+                  ... = r1 * i3 + (r2 * i3 + i1 * r3 + i2 * r3) :
+                        by rewrite *add.assoc
+                  ... = r1 * i3 + (i1 * r3 + r2 * i3 + i2 * r3) :
+                        by rewrite {i1 * r3 + _}add.comm
+                  ... = r1 * i3 + i1 * r3 + (r2 * i3 + i2 * r3) :
+                        by rewrite -*add.assoc)
+
+definition complex_distrib [instance] {A : Type} [r : comm_ring A] :
+                                      distrib (complex_ty A) :=
+  ⦃ distrib, left_distrib := complex_left_distrib,
+             right_distrib := complex_right_distrib ⦄
+
 /-
 theorem complex_mul_left_inv {A : Type} [f : field A] [g : group A] :
   ∀ (c : complex_ty A), complex_mul (complex_inv c) c = complex_one :=
